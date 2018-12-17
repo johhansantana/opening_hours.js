7d7314bc336131332621a4b4c7db48af0d56834a
Johhan Santana <johhan.santana@gmail.com>
2018-12-17 16:58:54 -0400

    fixing dependencies

--- locales/core.js
+++ locales/core.js
@@ -1,21 +1,25 @@
-import i18n from 'i18next-client';
+import i18n from "i18next";
 export default i18n;
 
-import opening_hours_resources from './opening_hours_resources.yaml';
+import opening_hours_resources from "./opening_hours_resources.yaml";
 
 if (!i18n.isInitialized()) {
-    i18n.init({
-        fallbackLng: 'en',
-        // lngWhitelist: ['en', 'de'],
-        resStore: opening_hours_resources,
-        getAsync: true,
-        useCookie: true,
-        // debug: true,
-    });
+  i18n.init({
+    fallbackLng: "en",
+    // lngWhitelist: ['en', 'de'],
+    resStore: opening_hours_resources,
+    getAsync: true,
+    useCookie: true
+    // debug: true,
+  });
 } else {
-    // compat with an app that already initializes i18n
-    for (var lang in opening_hours_resources) {
-        i18n.addResourceBundle(lang, 'opening_hours', opening_hours_resources[lang]['opening_hours'], true);
-
-    }
+  // compat with an app that already initializes i18n
+  for (var lang in opening_hours_resources) {
+    i18n.addResourceBundle(
+      lang,
+      "opening_hours",
+      opening_hours_resources[lang]["opening_hours"],
+      true
+    );
+  }
 }
